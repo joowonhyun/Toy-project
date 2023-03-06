4f5fec7dda3dd592be3f3a61f5f74495c8101cec
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2023-03-06 21:38:09 +0900

    Vanilla JS 페이지네이션 기능 구현

---
+++ pagination/index.js
@@ -0,0 +1,409 @@
+const lists = [
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+	{
+	 "title": "Lorem ipsum",
+	 "link": "https://www.naver.com",
+	 "date": "2023.01.30",
+	 "media": "매일경제",
+	 "lang": "KOR"
+	},
+ ]
+	const listEl = document.getElementById('list');
+	const paginationEl = document.getElementById('pagination');
+	for (let i = 0; i < lists.length; i++) {
+		let item = `<article>
+			<a href="${lists[i].link}" target="_blank">
+				<h2 class="title-post">${lists[i].title}</h2>
+			</a>
+			<div class="info">
+				<div>
+					<span class="date">${lists[i].date}</span>
+					<span class="lang">${lists[i].lang}</span>
+				</div>
+				<div class="media">${lists[i].media}</div>
+			</div>
+		</article>`;
+	
+		listEl.insertAdjacentHTML("beforeend", item);
+	}
+	const rows = document.querySelectorAll('#list article');
+	const rowsPerPage = 10; //한 페이지당 몇개 보여줄 지
+	const rowsCount = lists.length;
+	const pageCount = Math.ceil(rowsCount / rowsPerPage);
+	const numbers = document.querySelector('#pagination');
+	const prevBtn = document.querySelector('.prevBtn');
+	const nextBtn = document.querySelector('.nextBtn');
+	let pageAcitveIdx = 0; // 현재 보고 있는 페이지 그룹 번호
+	let maxPageNum = 3; // 페이지 그룹 최대 개수
+
+
+	for (let i = 1; i <= pageCount; i++) {
+		numbers.innerHTML += `<button>${i}</button>`
+		
+	}
+		const numberBtn = numbers.querySelectorAll('button');
+	
+	numberBtn.forEach((item, idx) => {
+		item.addEventListener('click', (e) => {
+			//테이블 출력함수
+			displayRow(idx);
+		})
+	});
+
+	function displayRow(idx) {
+		/*idx 0일때 : slice(0, 10);
+		idx 1일때 : slice(10, 20);*/
+
+		let start = idx * rowsPerPage;
+		let end = start + rowsPerPage;
+		let rowsArray = [...rows];
+		for (ra of rowsArray) {
+			ra.style.display = 'none';
+		}
+		let newRows = rowsArray.slice(start, end);
+		for (nr of newRows) {
+			nr.style.display = '';
+		}
+		for (nb of numberBtn) {
+			nb.classList.remove('active');
+		}
+		numberBtn[idx].classList.add('active');
+	}
+	displayRow(0);
+
+	function displayPage(num) {
+		//페이지네이션 번호 감추기
+		for (nb of numberBtn) {
+			nb.style.display = 'none';
+		}
+		// 몇 개의 페이지 그룹 생성
+		let totalPageCount = Math.ceil(pageCount / maxPageNum);
+
+		let pageArr = [...numberBtn];
+		let start = num * maxPageNum;
+		let end = start + maxPageNum;
+		let pargListArr = pageArr.slice(start, end);
+		for (let item of pargListArr) {
+			item.style.display = 'block';
+		}
+
+		if (pageAcitveIdx === 0) {
+			prevBtn.style.display = 'none'
+		} else {
+			prevBtn.style.display = 'block'
+		}
+
+		if (pageAcitveIdx === totalPageCount - 1) {
+			nextBtn.style.display = 'none'
+		} else {
+			nextBtn.style.display = 'block'
+		}
+	}
+	displayPage(0);
+
+	nextBtn.addEventListener('click', () => {
+		let nextPageNum = pageAcitveIdx * maxPageNum + maxPageNum;
+		displayRow(nextPageNum);
+		++pageAcitveIdx;
+		displayPage(pageAcitveIdx);
+	})
+
+	prevBtn.addEventListener('click', () => {
+		let nextPageNum = pageAcitveIdx * maxPageNum - maxPageNum;
+		displayRow(nextPageNum);
+		--pageAcitveIdx;
+		displayPage(pageAcitveIdx);
+	})
