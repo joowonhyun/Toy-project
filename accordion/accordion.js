d1303782c4b919823e8ec108db798af9076cf1b0
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2023-03-08 23:14:18 +0900

    Update accordion.js

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -2,14 +2,16 @@ window.onload = () => {
 	const panelFaqContainer = document.querySelectorAll('.panel-faq-container');
 	const panelFaqAnswer = document.querySelectorAll('.panel-faq-answer');
 	const btnAllClose = document.getElementById('btn-all-close');
-	
-	//반복문 순회하면서 해당 FAQ 제목 클릭시 콜백 처리
+
+	// //반복문 순회하면서 해당 FAQ 제목 클릭시 콜백 처리
 	for (let i = 0; i < panelFaqContainer.length; i++) {
-		panelFaqContainer[i].addEventListener('click', function() {
-			for (let j = 0; j < panelFaqContainer.length; j++) {
-				panelFaqAnswer[j].classList.remove('active');
+		panelFaqContainer[i].addEventListener('click', function () {
+			if (this.querySelector('.panel-faq-answer').classList.contains('active')) {
+				this.querySelector('.panel-faq-answer').classList.remove('active');
+			} else {
+				btnAllClose.click();
+				this.querySelector('.panel-faq-answer').classList.add('active');
 			}
-			this.querySelector('.panel-faq-answer').classList.toggle('active');
 		});
 	}
 	btnAllClose.addEventListener('click', () => {
