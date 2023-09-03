0401d8b9e38be670bd01331e058969738442784f
joowon <lkk7913@naver.com>
2023-09-03 19:20:53 +0900

    feat: 컴포넌트 업로드

---
+++ user-form/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./app.css";
+import FormInput from "./components/FormInput";
+
+const App = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "이름",
+      errorMessage:
+        "이름은 3~16자여야 하며 특수 문자를 포함할 수 없습니다!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "Email 주소는 필수 값입니다!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "비밀번호",
+      errorMessage:
+        "비밀번호는 8~20자 이내로 영문자 1개, 숫자 1개, 특수문자 1개 이상을 포함해야 합니다!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "비밀번호 확인",
+      errorMessage: "비밀번호가 일치하지 않습니다!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({...values, [e.target.name] : e.target.value})
+  }
+
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>회원가입</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
